ed60cea5e7cc4db768ce377cd661412bbe4efa5d
unknown <arthurrgspinto@gmail.com>
2025-12-01 13:00:53 -0300

    Correção nas routes do Cliente
    Durante um merge as rotas do clienteRoutes foram perdidas, e agora foram reintegradas.

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const clienteRoutes = express.Router();
+const telefoneRoutes = express.Router();
+const enderecoRoutes = express.Router();
 
-const { pedidoController } = require('../controllers/pedidoController');
+const { clienteController } = require('../controllers/clienteController');
 
-clienteRoutes.post('/clientes', pedidoController.addPedido);
-clienteRoutes.get('/clientes', pedidoController.selecionarPedido);
-clienteRoutes.get('/clientes/:cpfCliente', pedidoController.selecionarPedidoCliente);
-clienteRoutes.put('/clientes/:idCliente', pedidoController.atualizarPedido);
-clienteRoutes.delete('/clientes/:idCliente', pedidoController.cancelarPedido);
+clienteRoutes.post('/clientes', clienteController.adicionarCliente);
+clienteRoutes.get('/clientes', clienteController.buscarClientes);
+clienteRoutes.get('/clientes/:cpfCliente', clienteController.buscarClientePorCpf);
+clienteRoutes.put('/clientes/:idCliente', clienteController.atualizarCliente);
+clienteRoutes.delete('/clientes/:idCliente', clienteController.excluirCliente);
+
+const { telefoneController } = require('../controllers/clienteController');
+
+clienteRoutes.post('/telefones', telefoneController.adicionarTelefone);
+clienteRoutes.get('/telefones/:idClienteFK', telefoneController.buscarTelefoneCliente);
+clienteRoutes.get('/telefones', telefoneController.buscarTelefoneId);
+clienteRoutes.put('/telefones/:idTelefone', telefoneController.atualizarTelefone);
+clienteRoutes.delete('/telefones/:idTelefone', telefoneController.deletarTelefone);
+
+const { enderecoController } = require('../controllers/clienteController');
+
+clienteRoutes.get('/enderecos', enderecoController.buscarEnderecos);
+clienteRoutes.get('/enderecos/:idClienteFK', enderecoController.buscarEnderecoPorCliente); 
+clienteRoutes.post('/enderecos', enderecoController.adicionarEndereco);
+clienteRoutes.put('/enderecos/:idEndereco', enderecoController.atualizarEndereco); 
+clienteRoutes.delete('/enderecos/:idEndereco', enderecoController.excluirEndereco);
 
 
-module.exports =  clienteRoutes ;
+module.exports = { clienteRoutes, telefoneRoutes, enderecoRoutes }
